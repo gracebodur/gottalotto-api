4b316eb8f505bd937b2c8db8865a442e991fe1cc
Malek Haj-Hussein <malekadair@gmail.com>
2020-04-05 23:21:51 -0500

    Added: migrations seeds userRouter
    Co-authored-by: GraceBodur <mgracebodur@gmail.com>

---
+++ migrations/002.do.gottalotta.sql
@@ -0,0 +1,12 @@
+BEGIN;
+
+TRUNCATE
+  users;
+
+INSERT INTO users (user_name, full_name, password)
+VALUES
+  ('admin', 'Administrator Gary', '$2a$12$rugz4SDjwR.33.UL1fOlQ.qMmpZ5MfRQI6dXJi8w4vj526IFM5eou'),
+  ('malek', 'Malek Haj-Hussein', '$2a$12$3D/FNCVfI/RkbY.pw6HmbevZSFBBwxoHoIk/YTRVLQdIh9C0ECgYS'),
+  ('grace', 'Grace Bodur', '$2a$12$pOde1szWj5A6dGcODWCUY.KHFKqkMuGuUvRPDBm73FP9xwxxHEUlC');
+
+COMMIT;
--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const usersRouter = require('./users/users-router')
+
 
 const app = express()
 
@@ -13,14 +15,13 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!')
-})
+app.use('/api/users', usersRouter)
+
 
 app.use(function errorHandler(error, req, res, next) {
     let response
     if (NODE_ENV === 'production') {
-        response = { error: {message: 'server error'}}
+        response = { error: { message: 'server error' } }
     } else {
         console.error(error)
         response = { message: error.message, error }
