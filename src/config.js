30b3eb1c9d59851271c1a6157dccfef95a85727f
Malek Haj-Hussein <malekadair@gmail.com>
2020-04-17 15:47:24 -0500

    prepared for deployment part 2

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || "postgresql://malek_grace@localhost/gottalotto",
+    DATABASE_URL: process.env.DATABASE_URL || "postgresql://malek_grace@localhost/gottalotto",
     JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
     API_ENDPOINT: process.env.API_ENDPOINT || "http://localhost:8000/api"
 }
